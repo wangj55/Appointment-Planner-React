546db60542ff28c5e2d8649eed86b3770ae59d86
Ji Wang <wj42421999@gmail.com>
2022-05-08 03:42:35 -0400

    configure AppointmentForm

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,25 +1,19 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
-export const AppointmentForm = ({
-  contacts,
-  title,
-  setTitle,
-  contact,
-  setContact,
-  date,
-  setDate,
-  time,
-  setTime,
-  handleSubmit
-}) => {
+export const AppointmentForm = ({ contacts, title, setTitle, contact, setContact, date, setDate, time, setTime, handleSubmit }) => {
   const getTodayString = () => {
-    const [month, day, year] = new Date()
-      .toLocaleDateString("en-US")
-      .split("/");
+    const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input placeholder="title" type="text" value={title} onChange={({ target }) => setTitle(target.value)} />
+      <input placeholder="date" type="date" value={date} onChange={({ target }) => setDate(target.value)} min={getTodayString} />
+      <input placeholder="time" type="time" value={time} onChange={({ target }) => setTime(target.value)} />
+      <ContactPicker contacts={contacts} value={contact} onChange={({ target }) => setContact(target.value)} />
+      <input type="submit" />
+    </form>
   );
 };
