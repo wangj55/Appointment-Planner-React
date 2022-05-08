6678fc7e4cf1eb2bcead8b8d016db30e9a6955ea
Ji Wang <wj42421999@gmail.com>
2022-05-08 04:44:07 -0400

    Fix: syntax error in object's iteration

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      {tile.values.map((value, index) => {
+      {Object.values(tile).map((value, index) => {
         return (
           <p key={index} className={index === 0 ? "tile-title" : "tile"}>
             {value}
