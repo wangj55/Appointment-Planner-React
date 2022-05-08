5800fff5f28faba2e3e31ab1308f2c9c264da9c2
Ji Wang <wj42421999@gmail.com>
2022-05-08 03:43:11 -0400

    configure ContactPicker

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange }) => {
   return (
-    ContactPicker
+    <select onChange={onChange}>
+      <option key={1} value="not selected" selected="selected">
+        Select a contact
+      </option>
+      {contacts.map((contact, index) => {
+        <option key={index + 1} value={contact.name}></option>;
+      })}
+    </select>
   );
 };
