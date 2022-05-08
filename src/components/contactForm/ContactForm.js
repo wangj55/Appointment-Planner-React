d962d6ed07b5d04b31b2237eebd9bbaa17c6d077
Ji Wang <wj42421999@gmail.com>
2022-05-08 04:42:38 -0400

    Improve: format

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -3,10 +3,29 @@ import React from "react";
 export const ContactForm = ({ name, setName, phone, setPhone, email, setEmail, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Name" value={name} onChange={(target) => setName(target.value)} />
-      <input type="tel" placeholder="Phone" value={phone} onChange={(target) => setPhone(target.value)} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" />
-      <input type="email" placeholder="Email" value={email} onChange={(target) => setEmail(target.value)} />
-      <input type="submit" />
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <input
+        type="tel"
+        placeholder="Phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        required
+        pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input type="submit" value="Add Contact" />
     </form>
   );
 };
