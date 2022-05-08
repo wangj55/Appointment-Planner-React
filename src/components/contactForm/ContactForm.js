5f495ad5eda706af6121bfebdc45b8a16872f756
Ji Wang <wj42421999@gmail.com>
2022-05-08 01:27:05 -0400

    configure contactForm

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = ({ name, setName, phone, setPhone, email, setEmail, handleSubmit }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Name" value={name} onChange={(target) => setName(target.value)} />
+      <input type="tel" placeholder="Phone" value={phone} onChange={(target) => setPhone(target.value)} pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}" />
+      <input type="email" placeholder="Email" value={email} onChange={(target) => setEmail(target.value)} />
+      <input type="submit" />
+    </form>
   );
 };
