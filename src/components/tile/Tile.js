ed5916bb85ab11066070acac1ee76ee5406f686b
Ji Wang <wj42421999@gmail.com>
2022-05-08 01:27:31 -0400

    configure tile

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      
+      {tile.values.map((value, index) => {
+        return (
+          <p key={index} className={index === 0 ? "tile-title" : "tile"}>
+            {value}
+          </p>
+        );
+      })}
     </div>
   );
 };
