7c519910d4d89a1e988e7dbb980ba30977cd8c75
Ji Wang <wj42421999@gmail.com>
2022-05-08 04:42:25 -0400

    Improve: callback function

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { ContactPicker } from "../contactPicker/ContactPicker";
 
-export const AppointmentForm = ({ contacts, title, setTitle, contact, setContact, date, setDate, time, setTime, handleSubmit }) => {
+export const AppointmentForm = ({
+  contacts,
+  title,
+  setTitle,
+  contact,
+  setContact,
+  date,
+  setDate,
+  time,
+  setTime,
+  handleSubmit,
+}) => {
   const getTodayString = () => {
     const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
     return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
@@ -9,11 +20,31 @@ export const AppointmentForm = ({ contacts, title, setTitle, contact, setContact
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="title" type="text" value={title} onChange={({ target }) => setTitle(target.value)} />
-      <input placeholder="date" type="date" value={date} onChange={({ target }) => setDate(target.value)} min={getTodayString} />
-      <input placeholder="time" type="time" value={time} onChange={({ target }) => setTime(target.value)} />
-      <ContactPicker contacts={contacts} value={contact} onChange={({ target }) => setContact(target.value)} />
-      <input type="submit" />
+      <input
+        placeholder="title"
+        type="text"
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+      <input
+        placeholder="date"
+        type="date"
+        value={date}
+        onChange={({ target }) => setDate(target.value)}
+        min={getTodayString()}
+      />
+      <input
+        placeholder="time"
+        type="time"
+        value={time}
+        onChange={({ target }) => setTime(target.value)}
+      />
+      <ContactPicker
+        contacts={contacts}
+        value={contact}
+        onChange={({ target }) => setContact(target.value)}
+      />
+      <input type="submit" value="Add Appointment" />
     </form>
   );
 };
